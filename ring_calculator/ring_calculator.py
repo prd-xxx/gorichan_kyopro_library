aeacf109695da4528ab0e58c9bf18a5b7a620fde
prd-xxx <prd.kyopro@gmail.com>
2025-11-01 19:31:28 +0900

    codon 対応

--- ring_calculator/ring_calculator.py
+++ ring_calculator/ring_calculator.py
@@ -4,10 +4,10 @@ class RingCalculator:
         self.size = size
         self.base_index = base_index
     def _normalize(self, index):
-        return (index - self.base_index) % self.size
+        return (index - self.base_index + self.size) % self.size
     def dist_clockwise(self, from_index, to_index):
         fr = self._normalize(from_index)
         to = self._normalize(to_index)
-        return (to - fr) % self.size
+        return (to - fr + self.size) % self.size
     def dist_anticlockwise(self, from_index, to_index):
         return (self.size - self.dist_clockwise(from_index, to_index)) % self.size
